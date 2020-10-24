703334268d9f26224483e9505e5ee5fbc20d9d5f
Zcreator4 <zander.jimenez@hgs.hiddengeniusproject.org>
2020-10-23 18:25:33 -0700

    Section 11 vid 3

---
+++ Bmes/Models/Products/Brand.cs
@@ -0,0 +1,18 @@
+﻿using Bmes.Models.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bmes.Models.Products
+{
+    public class Brand : BaseObject
+    {
+        public string Name { get; set; }
+        public string Slug { get; set; }
+        public string Description { get; set; }
+        public string MetaDescription { get; set; }
+        public string MetaKeywords { get; set; }
+        public BrandStatus BrandStatus { get; set; }
+    }
+}
---
+++ Bmes/Models/Shared/BaseObject.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bmes.Models.Shared
+{
+    public class BaseObject
+    {
+        public long Id { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset ModifiedDate { get; set; }
+        public bool IsDeleted { get; set; }
+    }
+}
